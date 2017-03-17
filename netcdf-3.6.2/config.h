151b7fccd4cb6e44438f23050ce4232444a928dd
Wang-Hung Tse <simontse@bugaboo.westgrid.ca>
2017-03-16 17:36:24 -0700

    Replaced /home/simon by ${HOME} again in netcdf Makefile... wondering why the previous change was lost. Recompiled as well.

--- netcdf-3.6.2/config.h
+++ netcdf-3.6.2/config.h
@@ -55,7 +55,7 @@
 /* Define to 1 if you have the `strlcat' function. */
 /* #undef HAVE_STRLCAT */
 
-/* Define to 1 if `st_blksize' is a member of `struct stat'. */
+/* Define to 1 if `st_blksize' is member of `struct stat'. */
 #define HAVE_STRUCT_STAT_ST_BLKSIZE 1
 
 /* Define to 1 if your `struct stat' has `st_blksize'. Deprecated, use
@@ -135,9 +135,6 @@
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "netcdf"
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "3.6.2"
 
@@ -192,11 +189,6 @@
 # ifndef WORDS_BIGENDIAN
 /* #  undef WORDS_BIGENDIAN */
 # endif
-#endif
-
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
 #endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
