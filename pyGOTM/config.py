b2b41ca3eedefbff514f38f76052598e7d0ac069
Simon Tse <simon.tse@twu.ca>
2018-01-20 17:21:48 -0800

    new GOTM version v3d as default

--- pyGOTM/config.py
+++ pyGOTM/config.py
@@ -36,7 +36,7 @@ userhome = os.getenv('HOME')
 
 # Global "input" variables.
 project_name = 'medsea_GOTM'
-GOTM_version = 'v3c'
+GOTM_version = 'v3d'
 
 # Some essential GOTM settings overrider
 #timestep = 30
