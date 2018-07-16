bbd532094db75a3e4013cde8fa9023ffe9261e39
Simon Tse <simon.tse@twu.ca>
2018-07-15 22:29:26 -0700

    added "thrify=False" option to force computation on every lat/lon, good for interpolation on finer grids

--- pyGOTM/precompute.py
+++ pyGOTM/precompute.py
@@ -22,7 +22,7 @@ def date2ndaysnsecs(dt):
     nsecs = int((dt-datetime(dt.year,dt.month,dt.day,0,0,0)).total_seconds())
     return ndays, nsecs
 
-def medsea_ERA_append_cloud_factor(year): # Grid info used to get sea_mn
+def medsea_ERA_append_cloud_factor(year,month=None,thrifty=True): # Grid info used to get sea_mn
     """
     Append downward short wave radiation 3-hourly means to the nc variable 'swrd_cs' in each file.
     
@@ -32,7 +32,10 @@ def medsea_ERA_append_cloud_factor(year): # Grid info used to get sea_mn
     ## Preparations.    
 
     # Assumed filename pattern.
-    dst_fn = 'medsea_ERA_heat_{:d}.nc'.format(year)
+    if month is None:
+        dst_fn = 'medsea_ERA-INTERIM_heat_{:d}.nc'.format(year)
+    else:
+        dst_fn = 'medsea_ERA-INTERIM_heat_{:d}{:02d}.nc'.format(year,month)
     dst_folder = join(data_folder,'medsea_ERA-INTERIM')
     if not isdir(dst_folder) or not isfile(join(dst_folder,dst_fn)):
         raise(FileNotFoundError('The target directory must contain reformatted ERA data.'))
@@ -50,20 +53,38 @@ def medsea_ERA_append_cloud_factor(year): # Grid info used to get sea_mn
     # Grab some indices and lat/lon info about the medsea...
     # ... coz they are NOT available from ANY sort atmospheric forecast data.
     tic = time()
-    print('Precomputing swr_cs and cloud_factor values.')
-    for j,(m,n) in enumerate(medsea.sea_mn):
-        lat, lon = medsea.grid_lats[m],medsea.grid_lons[n]
-        #print('Calculating for lat,lon =',lat,',',lon)
-        for i,dt in enumerate(full_dates):
-            ndays, nsecs = date2ndaysnsecs(dt)
-            swrd_cs[i,m,n] = swr_3hourly_mean(ndays,nsecs,lat,lon)
-            if swrd_cs[i,m,n] > 0: # This implies fill_value for night time on sea locations. 
-                cloud_factor[i,m,n] = swrd[i,m,n]/swrd_cs[i,m,n] 
-        elapsed = time() - tic
-        avg_time = elapsed/(j+1)
-        J = medsea.sea_m.size
-        print('\t {:d}/{:d} completed. About {:.2f}s to go.'.format(j+1,medsea.sea_m.size,avg_time*(J-j-1)),end='\r')
-    print('\nElapsed {:.2f}s.'.format(elapsed))
+    if thrifty:
+        print('Precomputing swr_cs and cloud_factor values.')
+        for j,(m,n) in enumerate(medsea.sea_mn):
+            lat, lon = medsea.grid_lats[m],medsea.grid_lons[n]
+            #print('Calculating for lat,lon =',lat,',',lon)
+            for i,dt in enumerate(full_dates):
+                ndays, nsecs = date2ndaysnsecs(dt)
+                swrd_cs[i,m,n] = swr_3hourly_mean(ndays,nsecs,lat,lon)
+                if swrd_cs[i,m,n] > 0: # This implies fill_value for night time on sea locations. 
+                    cloud_factor[i,m,n] = swrd[i,m,n]/swrd_cs[i,m,n] 
+            elapsed = time() - tic
+            avg_time = elapsed/(j+1)
+            J = medsea.sea_m.size
+            print('\t {:d}/{:d} completed. About {:.2f}s to go.'.format(j+1,medsea.sea_m.size,avg_time*(J-j-1)),end='\r')
+        print('\nElapsed {:.2f}s.'.format(elapsed))
+    else:
+        print('Precomputing swr_cs and cloud_factor values.')        
+        j=0
+        J = medsea.M*medsea.N
+        for m in range(medsea.M):
+            for n in range(medsea.N):
+                lat, lon = medsea.grid_lats[m],medsea.grid_lons[n]                
+                for i,dt in enumerate(full_dates):
+                    ndays, nsecs = date2ndaysnsecs(dt)
+                    swrd_cs[i,m,n] = swr_3hourly_mean(ndays,nsecs,lat,lon)
+                    if swrd_cs[i,m,n] > 0: # This implies fill_value for night time on sea locations. 
+                        cloud_factor[i,m,n] = swrd[i,m,n]/swrd_cs[i,m,n] 
+                elapsed = time() - tic
+                j += 1
+                avg_time = elapsed/j
+                print('\t {:d}/{:d} completed. About {:.2f}s to go.'.format(j,J,avg_time*(J-j)),end='\r')
+        print('\nElapsed {:.2f}s.'.format(elapsed))
     
     # Append the data
     tic = time()
